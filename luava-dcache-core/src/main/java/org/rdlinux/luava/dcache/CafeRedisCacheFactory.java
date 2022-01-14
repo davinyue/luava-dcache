8489a73b7432711f04d50923744158226ea84eee
davinyue <davinyue@gmail.com>
2022-01-14 15:49:14 +0800

    优化工厂

--- luava-dcache-core/src/main/java/org/rdlinux/luava/dcache/CafeRedisCacheFactory.java
+++ luava-dcache-core/src/main/java/org/rdlinux/luava/dcache/CafeRedisCacheFactory.java
@@ -6,6 +6,7 @@ import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class CafeRedisCacheFactory {
     /**
@@ -30,20 +31,20 @@ public class CafeRedisCacheFactory {
     /**
      * 获取弱一致性双缓存
      */
-    public OpvCache getWeakConsistencyOpvDCache(String name, long timeout) {
+    public OpvCache getWeakConsistencyOpvDCache(String name, long timeout, TimeUnit timeUnit) {
         OpvCache dCache = this.dCacheOpvMap.get(name);
         if (dCache == null) {
             synchronized (this) {
                 dCache = this.dCacheOpvMap.get(name);
                 if (dCache == null) {
-                    CafeOpvBaseCache cafeCacheOpv = new CafeOpvBaseCache(timeout);
+                    CafeOpvBaseCache cafeCacheOpv = new CafeOpvBaseCache(timeUnit.toMillis(timeout));
                     RedisOpvBaseCache redisCacheOpv;
                     if (this.redisPrefix != null && !this.redisPrefix.isEmpty()) {
                         redisCacheOpv = new RedisOpvBaseCache(this.redisPrefix + ":" + name, this.redisTemplate);
                     } else {
                         redisCacheOpv = new RedisOpvBaseCache(name, this.redisTemplate);
                     }
-                    dCache = new WeakConsistencyOpvCache(name, timeout, this.redissonClient, cafeCacheOpv,
+                    dCache = new WeakConsistencyOpvCache(name, timeUnit.toMillis(timeout), this.redissonClient, cafeCacheOpv,
                             redisCacheOpv);
                     this.dCacheOpvMap.put(name, dCache);
                 }
@@ -55,17 +56,17 @@ public class CafeRedisCacheFactory {
     /**
      * 获取redis单缓存
      */
-    public OpvCache getRedisOpvCache(String name, long timeout) {
+    public OpvCache getRedisOpvCache(String name, long timeout, TimeUnit timeUnit) {
         OpvCache opvCache = this.cacheOpvMap.get(name);
         if (opvCache == null) {
             synchronized (this) {
                 opvCache = this.cacheOpvMap.get(name);
                 if (opvCache == null) {
                     if (this.redisPrefix != null && !this.redisPrefix.isEmpty()) {
-                        opvCache = new OpvSingleCache(name, timeout, this.redissonClient,
+                        opvCache = new OpvSingleCache(name, timeUnit.toMillis(timeout), this.redissonClient,
                                 new RedisOpvBaseCache(this.redisPrefix + ":" + name, this.redisTemplate));
                     } else {
-                        opvCache = new OpvSingleCache(name, timeout, this.redissonClient,
+                        opvCache = new OpvSingleCache(name, timeUnit.toMillis(timeout), this.redissonClient,
                                 new RedisOpvBaseCache(name, this.redisTemplate));
                     }
                     this.cacheOpvMap.put(name, opvCache);
@@ -78,13 +79,14 @@ public class CafeRedisCacheFactory {
     /**
      * 获取cafe单缓存
      */
-    public OpvCache getCafeOpvCache(String name, long timeout) {
+    public OpvCache getCafeOpvCache(String name, long timeout, TimeUnit timeUnit) {
         OpvCache opvCache = this.cacheOpvMap.get(name);
         if (opvCache == null) {
             synchronized (this) {
                 opvCache = this.cacheOpvMap.get(name);
                 if (opvCache == null) {
-                    opvCache = new OpvSingleCache(name, timeout, this.redissonClient, new CafeOpvBaseCache(timeout));
+                    long millis = timeUnit.toMillis(timeout);
+                    opvCache = new OpvSingleCache(name, millis, this.redissonClient, new CafeOpvBaseCache(millis));
                     this.cacheOpvMap.put(name, opvCache);
                 }
             }
